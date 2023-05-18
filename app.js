abc1bdfec10359c774e4b7351550543901c3d56f
IsmaelAxel <ismael_callamullo@hotmail.com>
2023-05-18 12:39:16 -0300

    Validacion por si quieren crear una tarea sin argumento o filtrar algo que no existe

--- app.js
+++ app.js
@@ -14,10 +14,18 @@ switch(true){
             titulo: argv[3],
             estado: "pendiente"
         }
+        if(crearTarea.titulo === undefined){
+            console.log(`No podes crear una tarea sin titulo`.red)
+            break
+        }
         console.log(`${archivo.guardarTarea(crearTarea.titulo)} -> ${crearTarea.estado}`.green)
     break
     case accion === "filtrar":
         estado = argv[3]
+        if(estado === undefined){
+            console.log(`No podemos filtrar algo que no estas especificando`.red)
+            break
+        }
         listadoDeTareasFiltrado = archivo.filtrarPorEstado(estado)
         console.log(`tareas en estado de: "${estado}"\n----------------`.rainbow)
         listadoDeTareasFiltrado.forEach((tarea)=>{
@@ -28,6 +36,6 @@ switch(true){
         console.log(`--------------------------\nNo entiendo qué quieres hacer\nLas acciones disponibles son: listar\n-----------------------------`.red)
     break
     default:
-        console.log(`Atención tienes que pasarme una acción\nLas acciones disponibles son: listar\n-----------------------------`.red)
+        console.log(`Atención tienes que pasarme una acción\nLas acciones disponibles son: listar, crear y filtrar\n-----------------------------`.red)
     break
 }
