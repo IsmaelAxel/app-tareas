eba3e33132db72cf6fa3f1a4564d01fbffa3defe
IsmaelAxel <ismael_callamullo@hotmail.com>
2023-05-17 21:28:29 -0300

    Agregamos la funcionalidad de crear tarea

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const archivo = require('./funcionesDeTareas')
 const argv = require('process').argv;
+require('colors')
 const accion = argv[2]
 switch(true){
     case accion === "listar":
         archivo.listar();
     break
-    case accion !== "listar"  && accion !== undefined:
-        console.log(`--------------------------\nNo entiendo qué quieres hacer\nLas acciones disponibles son: listar\n-----------------------------`)
+    case accion === "crear":
+        let crearTarea = {
+            titulo: argv[3],
+            estado: "pendiente"
+        }
+        console.log(`${archivo.guardarTarea(crearTarea.titulo)} -> ${crearTarea.estado}`.green)
+    break
+    case accion !== "listar" && accion !== "crear"  && accion !== undefined:
+        console.log(`--------------------------\nNo entiendo qué quieres hacer\nLas acciones disponibles son: listar\n-----------------------------`.red)
     break
     default:
-        console.log(`Atención tienes que pasarme una acción\nLas acciones disponibles son: listar\n-----------------------------`)
+        console.log(`Atención tienes que pasarme una acción\nLas acciones disponibles son: listar\n-----------------------------`.red)
     break
 }
--- funcionesDeTareas.js
+++ funcionesDeTareas.js
@@ -1,15 +1,29 @@
-const {readFileSync, /* writeFileSync */} = require('fs') /* Por ahora comentamos el writeFileSync porque no lo vamos usar (por ahora) */
+const {readFileSync,  writeFileSync } = require('fs') /* Por ahora comentamos el writeFileSync porque no lo vamos usar (por ahora) */
 /* Path es un modulo nativo de node que nos ayuda al manejo de rutas, en este caso usamos join que nos ayuda a pararnos en una ruta dinamica (de la mano de dirname) */
 const path = require('path') 
 const leerJSON = () =>JSON.parse(readFileSync(path.join(__dirname, 'tareas.json'), 'utf-8')) /* Con esto podemos leer el archivo tareas.json */
+const escribirJSON = (arrayDeTareas) => writeFileSync (path.join(__dirname, 'tareas.json'),JSON.stringify(arrayDeTareas, null, 3), 'utf-8');/* Con esto podemos sobrescribir el archivo tareas.json */
+const Tarea = function (id,titulo,estado){
+    this.id = id
+    this.titulo = titulo 
+    this.estado = estado
+}
 module.exports = {
     tareas: leerJSON(),
     listar: function(tareas = this.tareas){
-        console.log('Listado de tareas\n------------------')
+        console.log('Listado de tareas\n------------------'.rainbow)
         tareas.forEach(({id,titulo,estado}, i) => {
-            console.log(`${id}. ${titulo} - ${estado}`)
+            console.log(`${id}. ${titulo} - ${estado}`.green)
         });
     return null
-    } /* Con el metodo listar, pasamos como parametro predeterminado el atributo de this.tareas, que justamente lee el json, una vez haciendo eso.
+    },/* Con el metodo listar, pasamos como parametro predeterminado el atributo de this.tareas, que justamente lee el json, una vez haciendo eso.
      Hacemos un ForEach que recorra todo el json que ahora tiene formato de objeto literal. Por ultimo mostramos por consola todo lo que tenemos en el json */
+    guardarTarea: function(titulo){
+        let tarea = this.tareas
+        let ultimoId = tarea[tarea.length - 1] ? tarea[tarea.length - 1].id : 0;
+        let nuevaTarea = new Tarea(ultimoId + 1,titulo, estado = "pendiente")
+        tarea.push(nuevaTarea)
+        escribirJSON(tarea)
+        return `Nueva tarea creada\n----------------------\n${titulo}`
+    }
 } 
